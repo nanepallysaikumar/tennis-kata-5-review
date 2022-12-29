5ef98774e030937628338dcac6e0ecd131ef7a03
Sai Kumar <prasanna.kandregula.lpk@gmail.com>
2022-12-29 14:29:06 +0530

    Refractor: Moving the redundant test logic to the generic functions for the player scoring the point.

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -8,11 +8,20 @@ const {
   FIFTEEN_LOVE,
   THIRTY_LOVE,
   PLAYER_ONE,
+  ZERO,
+  ONE_TIME,
+  TWO_TIMES,
 } = testConstants;
 
 beforeEach(() => {
   render(<App />);
 });
+
+const playerOneScores = (times) => {
+  for (let count = ZERO; count < times; count++) {
+    fireEvent.click(screen.getByTestId(PLAYER_ONE));
+  }
+};
 
 const gameScoreShouldBe = (expected) => {
   expect(screen.getByTestId(GAME_SCORE_IDENTIFIER).textContent).toEqual(
@@ -25,14 +34,13 @@ test("When the Game Starts, then the initial score should be Love-All", () => {
 });
 
 test("When the running point of player one is 1 then the running score should be Fifteen-Love", () => {
-  fireEvent.click(screen.getByTestId(PLAYER_ONE));
+  playerOneScores(ONE_TIME);
 
   gameScoreShouldBe(FIFTEEN_LOVE);
 });
 
 test("When the running point of player one is 2 then the running score should be Thirty-Love", () => {
-  fireEvent.click(screen.getByTestId(PLAYER_ONE));
-  fireEvent.click(screen.getByTestId(PLAYER_ONE));
+  playerOneScores(TWO_TIMES);
 
   gameScoreShouldBe(THIRTY_LOVE);
 });
--- src/test/constants/testConstants.js
+++ src/test/constants/testConstants.js
@@ -4,6 +4,9 @@ const testConstants = {
   FIFTEEN_LOVE: "Fifteen-Love",
   PLAYER_ONE: "Player One",
   THIRTY_LOVE: "Thirty-Love",
+  ZERO: 0,
+  ONE_TIME: 1,
+  TWO_TIMES: 2,
 };
 
 export { testConstants };
