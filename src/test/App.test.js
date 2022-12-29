bdb43dd306d66a734869139ba495b4bccc6b3433
Sai Kumar <prasanna.kandregula.lpk@gmail.com>
2022-12-29 14:11:05 +0530

    Refractor: Moving the redundant test code into the generic functions.

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -5,18 +5,22 @@ import App from "../App";
 const { GAME_SCORE_IDENTIFIER, LOVE_ALL, FIFTEEN_LOVE, PLAYER_ONE } =
   testConstants;
 
-test("When the Game Starts, then the initial score should be Love-All", () => {
+beforeEach(() => {
   render(<App />);
+});
+
+const gameScoreShouldBe = (expected) => {
   expect(screen.getByTestId(GAME_SCORE_IDENTIFIER).textContent).toEqual(
-    LOVE_ALL
+    expected
   );
+};
+
+test("When the Game Starts, then the initial score should be Love-All", () => {
+  gameScoreShouldBe(LOVE_ALL);
 });
 
 test("When the running point of player one is 1 then the running score should be Fifteen-Love", () => {
-  render(<App />);
   fireEvent.click(screen.getByTestId(PLAYER_ONE));
 
-  expect(screen.getByTestId(GAME_SCORE_IDENTIFIER).textContent).toEqual(
-    FIFTEEN_LOVE
-  );
+  gameScoreShouldBe(FIFTEEN_LOVE);
 });
